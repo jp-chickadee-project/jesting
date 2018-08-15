0aaadaf2054fabcb738eda01b044654721143edb
Birdie <birdperson@gmail.com>
2018-08-15 19:40:21 -0400

    first commit, added basic test

---
+++ getSuite.test.js
@@ -0,0 +1,17 @@
+
+const supertest = require('supertest')
+const serverUrl = "euclid.nmu.edu:9200"
+const request = supertest(serverUrl)
+const path = "/api/birds/0700EE1E5D"
+
+describe("Get Request Suite.", async () => {
+	test("Get Birds", async () => {
+		let response = await request.get(path)
+		/* Test */
+		expect(response.status).toEqual(200)
+		expect(response.type).toEqual("application/json")
+		expect(response.body).toHaveProperty("rfid")
+		expect(response.body).toHaveProperty("bandCombo")
+		//console.log(response.body)
+	});
+});
