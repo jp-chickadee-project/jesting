285749d3f8403946a2f9c283cbb2e71db0541a80
Birdie <birdperson@gmail.com>
2018-08-15 21:05:29 -0400

    broken commit

--- getSuite.test.js
+++ getSuite.test.js
@@ -1,12 +1,13 @@
 
 const supertest = require('supertest')
-import config from './config';
+const config = require('./testConfig.js')
 const serverUrl = "euclid.nmu.edu:9200"
 const request = supertest(serverUrl)
 const path = "/api/birds/0700EE1E5D"
 
 describe("Get Request Suite.", async () => {
 	test("Get Birds", async () => {
+		console.log(config.port)
 		let response = await request.get(path)
 		/* Test */
 		expect(response.status).toEqual(200)
---
+++ testConfig.js
@@ -0,0 +1,6 @@
+const config = {
+	serverUrl: "euclid.nmu.edu:",
+	port: "9200" //refers to the api port
+};
+
+export default config;
