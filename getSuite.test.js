d54ff221a6f759678c726442e5d4aa29580f7a62
jonathonlefler <lefler.t.jonathon@gmail.com>
2018-08-16 10:56:16 -0400

    changed test order

--- getSuite.test.js
+++ getSuite.test.js
@@ -4,6 +4,13 @@ const config = require('./testConfig.js')
 const request = supertest(config.serverUrl + config.port)
 
 describe("Get Request Suite.", async () => {
+	test("Get all birds", async () => {
+		const getAllBirdsPath = "/api/birds"
+		let getAllBirdsResponse = await request.get(getAllBirdsPath)
+		/* Test */
+		expect(getAllBirdsResponse.status).toEqual(200)
+		expect(getAllBirdsResponse.type).toEqual("application/json")
+	});
 	test("Get bird by rfid", async () => {
 		const getBirdByRfidPath = "/api/birds/0700EE1E5D"
 		let getBirdByRfidResponse = await request.get(getBirdByRfidPath)
@@ -13,12 +20,5 @@ describe("Get Request Suite.", async () => {
 		expect(getBirdByRfidResponse.body).toHaveProperty("rfid")
 		expect(getBirdByRfidResponse.body).toHaveProperty("bandCombo")
 		//console.log(response.body)
-	});
-	test("Get all birds", async () => {
-		const getAllBirdsPath = "/api/birds"
-		let getAllBirdsResponse = await request.get(getAllBirdsPath)
-		/* Test */
-		expect(getAllBirdsResponse.status).toEqual(200)
-		expect(getAllBirdsResponse.type).toEqual("application/json")
 	});
 });
